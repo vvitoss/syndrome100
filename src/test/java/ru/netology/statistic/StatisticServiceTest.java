e7c989d70b93f91ca8982e61f2e17386aad3fedd
vvitoss <orangekaramel@gmail.com>
2022-01-24 19:31:11 +0300

    update Test findMaxIncrease

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -17,4 +17,17 @@ class StatisticServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIncrease() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
